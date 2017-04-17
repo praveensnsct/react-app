690d39b5ba02ec726ca85310e696117977bc7ec3
Praveen <praveensnsct@gmail.com>
2017-04-17 00:42:51 -0500

    Asc sorting in grid

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -7,12 +7,47 @@ import {GridBody, GridHeader} from './gridCompo';
 import './grid.css';
 
 export default class Grid extends Component {
-  render() {
-    return (
-      <div className="tableWrapper">
-        <GridHeader requiredFields={this.props.requiredFields} config={this.props.config}/>
-        <GridBody requiredFields={this.props.requiredFields} config={this.props.config} data={this.props.data}/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: props.data
+        };
+        // This binding is necessary to make `this` work in the callback
+        this.sortData = this.sortData.bind(this);
+    }
+
+    sortData(key, order, type) {
+        let data = this.state.data;
+        if (type === 'int') {
+            data.sort(function (a, b) {
+                if (order === 'asc') {
+                    return a[key] ? a[key] : 0 - b[key] ? b[key] : 0;
+                } else {
+                    return b[key] ? b[key] : 0 - a[key] ? a[key] : 0;
+                }
+            });
+        } else if (type === 'string') {
+            data.sort(function (a, b) {
+                let x = a[key] ? a[key].toString().toLowerCase() : '';
+                let y = b[key] ? b[key].toString().toLowerCase() : '';
+                if (order === 'asc')
+                    return x < y ? -1 : x > y ? 1 : 0;
+                else
+                    return x < y ? 1 : x > y ? -1 : 0;
+            });
+        }
+        this.setState({
+            data: data
+        });
+    }
+
+    render() {
+        return (
+            <div className="tableWrapper">
+                <GridHeader clickIt={this.sortData} requiredFields={this.props.requiredFields}
+                            config={this.props.config}/>
+                <GridBody requiredFields={this.props.requiredFields} config={this.props.config} data={this.props.data}/>
+            </div>
+        );
+    }
 }
--- src/components/grid/gridCompo.js
+++ src/components/grid/gridCompo.js
@@ -5,61 +5,70 @@
 import React, {Component} from 'react';
 
 export class GridSearch extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
 }
 
 export class GridHeader extends Component {
-  render() {
-    return (
-      <div className="grid__header">
-        <table>
-          <tbody>
-            <TableRows fields={this.props.requiredFields} config={this.props.config} type="header" className="table__header_text"/>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="grid__header">
+                <table>
+                    <tbody>
+                    <TableRows clickIt={this.props.clickIt} fields={this.props.requiredFields}
+                               config={this.props.config}
+                               type="header"
+                               className="table__header_text"/>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export class GridBody extends Component {
-  render() {
-    return (
-      <div className="grid__body">
-        <table>
-          <tbody>
-            {this.props.data.map((data, idx) => <TableRows key={idx} fields={this.props.requiredFields} config={this.props.config}
-                                                           data={data} type="body" className="table__data_text"/>)}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="grid__body">
+                <table>
+                    <tbody>
+                    {this.props.data.map((data, idx) => <TableRows key={idx} fields={this.props.requiredFields}
+                                                                   config={this.props.config}
+                                                                   data={data} type="body"
+                                                                   className="table__data_text"/>)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 class TableRows extends Component {
-  render() {
-    if(this.props.type === 'header') {
-      return (
-        <tr>
-          {this.props.fields.map((field, idx) => <td className={this.props.className} key={idx} style={{width:this.props.config[field].grid.width || 150}}>
-            {this.props.config[field].label}
-          </td>)}
-        </tr>
-      )
-    }else if(this.props.type === 'body'){
-      return (
-        <tr>
-          {this.props.fields.map((field, idx) => <td className={this.props.className} key={idx} style={{width:this.props.config[field].grid.width || 150}}>
-            {this.props.data[field]}
-          </td>)}
-        </tr>
-      )
+    render() {
+        if (this.props.type === 'header') {
+            return (
+                <tr>
+                    {this.props.fields.map((field, idx) => <td
+                        onClick={() => this.props.clickIt(field, 'asc', this.props.config[field].grid.type)} id={field}
+                        className={this.props.className} key={idx}
+                        style={{width: this.props.config[field].grid.width || 150}}>
+                        {this.props.config[field].label}
+                    </td>)}
+                </tr>
+            )
+        } else if (this.props.type === 'body') {
+            return (
+                <tr>
+                    {this.props.fields.map((field, idx) => <td className={this.props.className} key={idx}
+                                                               style={{width: this.props.config[field].grid.width || 150}}>
+                        {this.props.data[field]}
+                    </td>)}
+                </tr>
+            )
+        }
     }
-  }
 }
